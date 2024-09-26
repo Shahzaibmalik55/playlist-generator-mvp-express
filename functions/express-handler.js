c58c41baa66a1c2a7433702208364fc57ffd1279
shahzaib2819 <shahzaib@devdksa.com>
2024-09-26 13:35:51 +0500

    git remove netlify config

--- functions/express-handler.js
+++
@@ -1,4 +0,0 @@
-const serverless = require("serverless-http");
-const app = require("../src/index"); // Adjust the path to your `index.js` file
-
-module.exports.handler = serverless(app);
--- src/index.js
+++ src/index.js
@@ -14,8 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
 
-// app.listen(ServerConfig.PORT, () => {
-//   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-// });
-
-module.exports = app;
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+});
